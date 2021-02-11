0f3bbd07c84868e17d00ec51072b3fa6c68cc8f6
Argedik <gedikas@hotmailc.om>
2021-02-11 13:18:03 +0300

    Update yukle.dart

--- lib/sayfalar/yukle.dart
+++ lib/sayfalar/yukle.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Yukle extends StatefulWidget {
   @override
@@ -6,8 +9,78 @@ class Yukle extends StatefulWidget {
 }
 
 class _YukleState extends State<Yukle> {
+  File dosya;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Yükleme Sayfası"));
+    return dosya == null ? yukleButonu() : gonderiFormu();
+  }
+
+  Widget yukleButonu() {
+    return IconButton(
+      icon: Icon(Icons.file_upload, size: 50.0),
+      onPressed: () {
+        fotografSec();
+      },
+    );
+  }
+
+  Widget gonderiFormu() {
+    return Center(child: Text("Yüklenen resim ve text alanları gelecek."));
+  }
+
+  fotografSec() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text("Gönderi Oluştur"),
+          children: [
+            SimpleDialogOption(
+              child: Text("Fotoğraf Çek"),
+              onPressed: () {
+                fotoCek();
+              },
+            ),
+            SimpleDialogOption(
+              child: Text("Galeriden  Yükle"),
+              onPressed: () {},
+            ),
+            SimpleDialogOption(
+              child: Text("İptal"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  fotoCek() async {
+    Navigator.pop(context);
+    var image = await ImagePicker().getImage(
+      source: ImageSource.camera,
+      maxWidth: 800,
+      maxHeight: 600,
+      imageQuality: 80,
+    );
+    setState(() {
+      dosya = File(image.path);
+    });
+  }
+
+  galeridenSec() async {
+    Navigator.pop(context);
+    var image = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 800,
+      maxHeight: 600,
+      imageQuality: 80,
+    );
+    setState(() {
+      dosya = File(image.path);
+    });
   }
 }
